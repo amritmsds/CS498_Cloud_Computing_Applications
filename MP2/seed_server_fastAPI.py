0de78d682a8334c09cb18d1d316ae1e4153848db
pandeyamrit <42584711+pandeyamrit@users.noreply.github.com>
2024-01-28 20:49:03 -0500

    MP2 initial code checkin
    Initial checkin

---
+++ MP2/seed_server_fastAPI.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+
+# Initial seed value
+seed_value = 0
+
+@app.get("/")
+def get_seed():
+    return str(seed_value)
+
+@app.post("/")
+def update_seed(num: int):
+    global seed_value
+    seed_value = num
+    return {"message": f"Seed value updated successfully to '{num}'."}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8081)
